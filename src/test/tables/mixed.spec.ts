f3f8548eb0ba75b7f7d6960d0765f59954989aa8
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-11-29 12:16:54 +0100

    Added GFM tests

--- src/test/tables/mixed.spec.ts
+++ src/test/tables/mixed.spec.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 import { HTMLTableParser, HTMLTableRenderer } from "../../tables/htmlTable.js";
 import { MultiMarkdownTableParser, PrettyMultiMarkdownTableRenderer } from "../../tables/multiMarkdownTable.js";
 import { Table } from "../../tables/table.js";
+import { GitHubFlavoredMarkdownTableParser, GitHubFlavoredMarkdownTableRenderer } from "../../tables/gfmTable.js";
 
 interface TableTest {
     description: string,
@@ -41,6 +42,7 @@ function readTablesFile(fileName: string, hasOutput: boolean) {
     return tests;
 }
 
+/* TODO: Test MinifiedMultiMarkdownTableRenderer */
 describe("Mixed Multimarkdown test (HTMLTableParser, HTMLTableRenderer, MultiMarkdownTableParser, PrettyMultiMarkdownTableRenderer)", () => {
     let htmlParser: HTMLTableParser;
     let htmlPrettyRenderer: HTMLTableRenderer;
@@ -159,6 +161,132 @@ describe("Mixed Multimarkdown test (HTMLTableParser, HTMLTableRenderer, MultiMar
             it(test.description, () => {
                 expect(() => {
                     mmdParser.parse(test.mdInput);
+                }).to.throw();
+            });
+        }
+
+    });
+});
+
+
+describe("Mixed GitHub-Flavored-Markdown test (HTMLTableParser, HTMLTableRenderer, GitHubFlavoredMarkdownTableParser, GitHubFlavoredMarkdownTableRenderer)", () => {
+    let htmlParser: HTMLTableParser;
+    let htmlPrettyRenderer: HTMLTableRenderer;
+    let mdParser: GitHubFlavoredMarkdownTableParser;
+    let mdPrettyRenderer: GitHubFlavoredMarkdownTableRenderer;
+
+    before(() => {
+        htmlParser = new HTMLTableParser();
+        htmlPrettyRenderer = new HTMLTableRenderer(true, " ".repeat(4));
+        mdParser = new GitHubFlavoredMarkdownTableParser();
+        mdPrettyRenderer = new GitHubFlavoredMarkdownTableRenderer();
+    });
+
+    describe("results should match the given tables from the *.txt file", () => {
+
+        const tests = readTablesFile("valid-gfm-tables.txt", true);
+
+        context("GFM -> HTML", () => {
+            for (const test of tests) {
+                it(test.description, () => {
+                    // Parse the table:
+                    let intermediaryTable: Table;
+                    expect(() => {
+                        intermediaryTable = mdParser.parse(test.mdInput);
+                        intermediaryTable.update();
+                    }).to.not.throw();
+
+                    // Render the table:
+                    let htmlOutput = htmlPrettyRenderer.render(intermediaryTable);
+                    expect(
+                        htmlOutput.replace(/[ \t]{2,}/g, " ")
+                    ).to.equal(test.htmlOutput.replace(/[ \t]{2,}/g, " "));
+                });
+            }
+        });
+
+        context("GFM -> GFM", () => {
+            for (const test of tests) {
+                it(test.description, () => {
+                    // Parse the table:
+                    let intermediaryTable: Table;
+                    expect(() => {
+                        intermediaryTable = mdParser.parse(test.mdInput);
+                        intermediaryTable.update();
+                    }).to.not.throw();
+
+                    // Render the table:
+                    let mdOutput = mdPrettyRenderer.render(intermediaryTable);
+                    expect(mdOutput).to.equal(test.mdOutput);
+                });
+            }
+        });
+
+        context("GFM -> HTML -> HTML", () => {
+            for (const test of tests) {
+                it(test.description, () => {
+                    let htmlOutput;
+                    try {
+                        // Parse the table:
+                        let intermediaryTable: Table;
+                        intermediaryTable = mdParser.parse(test.mdInput);
+                        intermediaryTable.update();
+    
+                        // Render the table:
+                        htmlOutput = htmlPrettyRenderer.render(intermediaryTable);
+                    } catch { }
+
+                    if (htmlOutput) {
+                        // Parse the html output and render it again:
+                        expect(
+                            htmlPrettyRenderer.render(
+                                htmlParser.parse(htmlOutput)
+                            ).replace(/[ \t]{2,}/g, " ")
+                        ).to.equal(
+                            test.htmlOutput.replace(/[ \t]{2,}/g, " ")
+                        );
+                    }
+                });
+            }
+        });
+
+        context("GFM -> GFM -> GFM", () => {
+            for (const test of tests) {
+                it(test.description, () => {
+                    let mdOutput;
+                    try {
+                        // Parse the table:
+                        let intermediaryTable: Table;
+                        intermediaryTable = mdParser.parse(test.mdInput);
+                        intermediaryTable.update();
+    
+                        // Render the table:
+                        mdOutput = mdPrettyRenderer.render(intermediaryTable);
+                    } catch { }
+
+                    if (mdOutput) {
+                        // Parse the mmd output and render it again:
+                        expect(
+                            mdPrettyRenderer.render(
+                                mdParser.parse(mdOutput)
+                            )
+                        ).to.equal(
+                            test.mdOutput
+                        );
+                    }
+                });
+            }
+        });
+    });
+
+    describe("invalid tables from the *.txt file should throw ParsingError", () => {
+
+        const tests = readTablesFile("invalid-gfm-tables.txt", false);
+
+        for (const test of tests) {
+            it(test.description, () => {
+                expect(() => {
+                    mdParser.parse(test.mdInput);
                 }).to.throw();
             });
         }
